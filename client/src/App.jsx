a52ae503c09ebaa4f342f9251ec3f2515c636189
Peter Solomon <petersolomon704@gmail.com>
2022-07-14 22:56:58 +0100

    Added top grid

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import "./App.css"
+import Home from './componments/pages/home/Home'
 
 const App = () => {
   return (
     <div className='App'>
       <div className="blur" style={{top : "-18%", right : "0"}}></div>
       <div className="blur" style={{top : "-36%", left : "-8"}}></div>
+      <Home/>
     </div>
   )
 }
---
+++ client/src/componments/pages/home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import "../home/Home.css"
+
+const Home = () => {
+  return (
+
+        <div className="home">
+            <div className="profile-home">Profile</div>
+            <div className="post-homne">Post</div>
+            <div className="riht-home">Right bar</div>
+        </div>
+
+  )
+}
+
+export default Home
