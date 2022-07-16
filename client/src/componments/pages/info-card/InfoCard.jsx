341fce96e75e91709c8acadf83ea58c311889750
Peter Solomon <petersolomon704@gmail.com>
2022-07-16 13:47:50 +0100

    Modified logout in profile left

--- client/src/componments/pages/info-card/InfoCard.jsx
+++ client/src/componments/pages/info-card/InfoCard.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
+import React from "react";
 
-import "../info-card/InfoCard.css"
+import "../info-card/InfoCard.css";
+import { UilPen } from "@iconscout/react-unicons";
+import { UilSignOut } from "@iconscout/react-unicons";
 
 const InfoCard = () => {
   return (
-    <div>
-      <h3>Info card</h3>
-    </div>
-  )
-}
+    <div className="info-card">
+      <div className="info-heading">
+        <h3>Your info -</h3>
+        <UilPen  style ={{color : "#fff"}} />
+      </div>
+<div className="info">
+    <span> <strong>Marital Status</strong> </span>
+    <span className="info-desc"> <strong>Married</strong> </span>
+</div>
+<div className="info">
+    <span> <strong>Location</strong> </span>
+    <span className="info-desc"> <strong>Lagos</strong> </span>
+</div>
+<div className="info">
+    <span> <strong>Works @</strong> </span>
+    <span className="info-desc"> <strong>Deplatform Tech</strong> </span>
+</div>
+<div className="info">
+    <span> <strong>Likes</strong> </span>
+    <span className="info-desc"> <strong>Reading | Music | Writing</strong> </span>
+</div>
 
-export default InfoCard
+<i class="fa-solid fa-arrow-right-from-bracket"></i>
+<button className="button log-out">LogOut</button>
+
+
+    </div>
+  );
+};
+
+export default InfoCard;
