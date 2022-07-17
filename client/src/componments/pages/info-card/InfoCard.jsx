5cb58b9bdc369959360310dc673777b96a793570
Peter Solomon <petersolomon704@gmail.com>
2022-07-17 18:05:43 +0100

    Configured modal info

--- client/src/componments/pages/info-card/InfoCard.jsx
+++ client/src/componments/pages/info-card/InfoCard.jsx
@@ -1,37 +1,67 @@
 import React from "react";
+import { useState } from "react";
 
 import "../info-card/InfoCard.css";
 import { UilPen } from "@iconscout/react-unicons";
+import ProfileModal from "../profile-modal/ProfileModal";
+
 
 const InfoCard = () => {
+  const [modalOpened, setModalopened] = useState(false);
   return (
     <div className="info-card">
       <div className="info-heading">
         <h3>Your info -</h3>
-        <UilPen  style ={{color : "#fff"}} />
-        <div>MODAL</div>
+        <UilPen
+          style={{ color: "#fff", cursor : "pointer" }}
+          onClick={() => setModalopened(true)}
+        />
+      
+        <ProfileModal modalOpened={modalOpened} setModalOpened ={setModalopened} />
       </div>
-<div className="info">
-    <span> <strong>Marital Status</strong> </span>
-    <span className="info-desc"> <strong>Married</strong> </span>
-</div>
-<div className="info">
-    <span> <strong>Location</strong> </span>
-    <span className="info-desc"> <strong>Lagos</strong> </span>
-</div>
-<div className="info">
-    <span> <strong>Works @</strong> </span>
-    <span className="info-desc"> <strong>Deplatform Tech</strong> </span>
-</div>
-<div className="info">
-    <span> <strong>Likes</strong> </span>
-    <span className="info-desc"> <strong>Reading | Music | Writing</strong> </span>
-</div>
+      <div className="info">
+        <span>
+          {" "}
+          <strong>Marital Status</strong>{" "}
+        </span>
+        <span className="info-desc">
+          {" "}
+          <strong>Married</strong>{" "}
+        </span>
+      </div>
+      <div className="info">
+        <span>
+          {" "}
+          <strong>Location</strong>{" "}
+        </span>
+        <span className="info-desc">
+          {" "}
+          <strong>Lagos</strong>{" "}
+        </span>
+      </div>
+      <div className="info">
+        <span>
+          {" "}
+          <strong>Works @</strong>{" "}
+        </span>
+        <span className="info-desc">
+          {" "}
+          <strong>Deplatform Tech</strong>{" "}
+        </span>
+      </div>
+      <div className="info">
+        <span>
+          {" "}
+          <strong>Likes</strong>{" "}
+        </span>
+        <span className="info-desc">
+          {" "}
+          <strong>Reading | Music | Writing</strong>{" "}
+        </span>
+      </div>
 
-<i class="fa-solid fa-arrow-right-from-bracket"></i>
-<button className="button log-out">LogOut</button>
-
-
+      <i class="fa-solid fa-arrow-right-from-bracket"></i>
+      <button className="button log-out">LogOut</button>
     </div>
   );
 };
--- client/src/componments/pages/profile-modal/ProfileModal.jsx
+++ client/src/componments/pages/profile-modal/ProfileModal.jsx
@@ -1,20 +1,29 @@
+import React from "react";
 
+import { Modal, useMantineTheme } from "@mantine/core";
 
-import { Modal, useMantineTheme } from '@mantine/core';
-
-const Demo =()=> {
+const ProfileModal = ({modalOpened, setModalOpened}) => {
   const theme = useMantineTheme();
+
+  const handleFormSubmit = () => {};
 
   return (
     <Modal
-      overlayColor={theme.colorScheme === 'dark' ? theme.colors.dark[9] : theme.colors.gray[2]}
+      overlayColor={
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[9]
+          : theme.colors.gray[2]
+      }
       overlayOpacity={0.55}
       overlayBlur={3}
+      opened = {modalOpened}
+      onClose = { () => setModalOpened(false)}
     >
-      {/* Modal content */}
+      <form className="infoForm" onSubmit={handleFormSubmit}>
+        <h3>MODAL</h3>
+      </form>
     </Modal>
   );
-}
+};
 
-
-export default Demo
+export default ProfileModal;
