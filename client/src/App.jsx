827460020c58f222f9cc19c0a7a1090e84571585
Peter Solomon <petersolomon704@gmail.com>
2022-07-20 11:59:36 +0100

    Configured auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import Home from "./componments/pages/home/Home";
+import Auth from "./componments/screens/auth/Auth";
 import ProfileMain from "./componments/screens/profileScreen/Profile-main/ProfileMain";
+import { ToastContainer, Zoom } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Login from "./componments/screens/user/login/Login";
 import Register from "./componments/screens/user/register/Register";
 
@@ -11,15 +14,14 @@ const App = () => {
     <div className="App">
       {/* <div className="blur" style={{ top: "-18%", right: "0" }}></div>
       <div className="blur" style={{ top: "-36%", left: "-8" }}></div> */}
+      <ToastContainer draggable={false} transition={Zoom} autoClose={3000} />
       <Routes>
-
-        <Route  path="/" element ={ <Home/> } />
-        <Route  path="/profile" element ={ <ProfileMain/> } />
-        <Route  path="/login" element ={ <Login/> } />
-        <Route  path="/register" element ={ <Register/> } />
-
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<ProfileMain />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/auth" element={<Auth />} />
       </Routes>
-   
     </div>
   );
 };
---
+++ client/src/componments/screens/auth/Auth.jsx
@@ -0,0 +1,230 @@
+import React from "react";
+import { useState } from "react";
+import { Boostar101Logo } from "../../../data/followers";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import "../auth/Auth.css";
+const initialState = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  username: "",
+  password: "",
+  confirmPassword: ""
+};
+
+const Auth = () => {
+  const [isSignUp, setIsSignUp] = useState(true);
+  const [formData, setFormData] = useState(initialState);
+  const {
+    firstName,
+    lastName,
+    email,
+    username,
+    password,
+    confirmPassword
+  } = formData;
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const handleChange = e => {
+    let { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  const handleFormSubmit = e => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      return toast.error("Password not matched");
+    }
+
+    if (
+      firstName &&
+      lastName &&
+      email &&
+      username &&
+      password &&
+      confirmPassword
+    ) {
+      dispatch(signUpAction({ formData, navigate, toast }));
+      setFormData(initialState);
+    } else {
+      if (username && password) {
+        dispatch(logInAction({ formData, navigate,toast }));
+      }
+    }
+
+  };
+  return (
+    <div className="container px-4">
+      <div
+        className="row"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh"
+        }}
+      >
+        <div
+          className="col-md-6"
+          style={{
+            borderRadius: "10px"
+          }}
+        >
+          <div className="left-side">
+            <div className="left-image">
+              <img
+                src={Boostar101Logo}
+                alt=""
+                width="100"
+                className="boostar-logo"
+              />
+            </div>
+
+            <div className="discription">
+              <h3>Welcome To Boostar 101.com</h3>
+              <h6>https://boostar101.com</h6>
+              <p>Inspire | Empower | Motivate</p>
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="col-md-6"
+          style={{
+            backgroundColor: "#85a5ff",
+            padding: "40px",
+            borderRadius: "10px"
+          }}
+        >
+          <form className="infoForm" onSubmit={handleFormSubmit}>
+            <div className="sign-up">
+              <h3> {isSignUp ? "Sign Up" : "Log In"}</h3>{" "}
+            </div>
+            {isSignUp && (
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="input-container">
+                    {" "}
+                    <input
+                      required
+                      type="text"
+                      placeholder="First Name"
+                      className="infoInput"
+                      name="firstName"
+                      value={firstName}
+                      onChange={handleChange}
+                    />
+                  </div>{" "}
+                </div>
+                <div className="col-md-6">
+                  {" "}
+                  <div className="input-container">
+                    {" "}
+                    <input
+                      required
+                      type="text"
+                      placeholder="Last Name"
+                      className="infoInput"
+                      name="lastName"
+                      value={lastName}
+                      onChange={handleChange}
+                    />
+                  </div>{" "}
+                </div>
+              </div>
+            )}
+
+            <div className="row">
+              <div className="col-md-12">
+                {" "}
+                <div className="input-container">
+                  {" "}
+                  <input
+                    required
+                    type="email"
+                    placeholder="Email"
+                    className="infoInput"
+                    name="email"
+                    value={email}
+                    onChange={handleChange}
+                  />
+                </div>{" "}
+              </div>
+              {isSignUp && (
+                <div className="col-md-12">
+                  <div className="input-container">
+                    <input
+                      required
+                      type="text"
+                      placeholder="User Name"
+                      className="infoInput"
+                      name="username"
+                      value={username}
+                      onChange={handleChange}
+                    />
+                  </div>{" "}
+                </div>
+              )}
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                {" "}
+                <div className="input-container-2">
+                  <input
+                    required
+                    type="password"
+                    placeholder="password"
+                    className="infoInput"
+                    name="password"
+                    value={password}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+            </div>
+            {isSignUp && (
+              <div className="row">
+                <div className="col-md-12">
+                  {" "}
+                  <div className="input-container-2">
+                    <input
+                      required
+                      type="password"
+                      placeholder="Confirm Paasword"
+                      className="infoInput"
+                      name="confirmPassword"
+                      value={confirmPassword}
+                      onChange={handleChange}
+                    />
+                  </div>{" "}
+                </div>
+              </div>
+            )}
+
+            <div>
+              <button className="button submit-button" type="submit">
+                {isSignUp ? "Sign Up" : "Log In"}
+              </button>
+            </div>
+            <span
+              className="have-account"
+              onClick={() => {
+                setIsSignUp(prev => !prev);
+              }}
+            >
+              {isSignUp
+                ? "Already have an account | Login"
+                : "Don't have an account | Sign up"}
+            </span>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
